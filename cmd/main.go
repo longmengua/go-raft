feb9cc59ee2623a8b0ba88ced10363fea74ce13b
Waltor <longmengua@users.noreply.github.com>
2025-07-15 15:09:48 +0800

    fix: fix main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"go-raft/internal/adapters/http"
 	"go-raft/internal/adapters/http/asset"
 	"go-raft/internal/raft"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	// Initialize the Raft store
 	raftstore, err := raft.New()
 	if err != nil {
@@ -18,10 +25,14 @@ func main() {
 	assethandler := asset.NewHanlder(raftstore.NodeHost, raftstore.ClusterID)
 
 	// [::1]:19090 for ipv6
-	httpserver := http.New([]string{":19090", "0.0.0.0:9090", "[::1]:9090"}, assethandler)
+	httpserver := http.New([]string{"0.0.0.0:9090"}, assethandler)
 	go func() {
 		if err := httpserver.Start(); err != nil {
 			log.Fatalf("failed to start HTTP server: %v", err)
 		}
 	}()
+
+	// 等待中斷訊號
+	<-ctx.Done()
+	log.Println("Main: shutdown signal received")
 }
